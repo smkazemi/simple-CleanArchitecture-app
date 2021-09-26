9aeac0fb69a03c01ba523727e6e6824b3aa3c68b
smr <sm.kazemy73@gmail.com>
2021-09-26 19:38:25 +0330

    fix ssl Exception

--- app/src/main/java/com/basalam/intern/android/App.kt
+++ app/src/main/java/com/basalam/intern/android/App.kt
@@ -1,11 +1,19 @@
 package com.basalam.intern.android
 
-import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.basalam.intern.android.di.AppComponent
 import com.basalam.intern.android.di.DaggerAppComponent
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
 import timber.log.Timber
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import javax.net.ssl.SSLContext
 
-class App : Application() {
+class App : MultiDexApplication() {
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(applicationContext)
@@ -14,10 +22,32 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        try {
+            // Google Play will install latest OpenSSL
+            ProviderInstaller.installIfNeeded(applicationContext)
+            val sslContext: SSLContext = SSLContext.getInstance("TLSv1.2")
+            sslContext.init(null, null, null)
+            sslContext.createSSLEngine()
+
+        } catch (e: GooglePlayServicesRepairableException) {
+            e.printStackTrace()
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: KeyManagementException) {
+            e.printStackTrace()
+        }
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
 
+    }
 
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
